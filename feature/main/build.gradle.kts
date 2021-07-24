38ee2c552532c2f77c0236bf3895926c6e00603f
Go Takahana <tagahana508@gmail.com>
2021-07-24 11:20:14 +0900

    navigate to RunWhenFragmentClosedFragment

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
+  id("com.android.library")
+  id("kotlin-android")
+  id("kotlin-kapt")
+  id("dagger.hilt.android.plugin")
 }
 
 apply(from = rootProject.file("android_config.gradle"))
 
 dependencies {
-    
-    implementation(libs.hilt.android.core)
-    kapt(libs.hilt.android.compiler)
 
-    implementation(projects.shared.compose)
-    implementation(projects.shared.feature)
+  implementation(libs.hilt.android.core)
+  kapt(libs.hilt.android.compiler)
+  implementation(libs.androidx.nav.fragment)
+  implementation(libs.androidx.nav.ui)
+
+  implementation(projects.feature.runwhenfragmentclosed)
+  implementation(projects.shared.compose)
+  implementation(projects.shared.feature)
 }
--- feature/main/src/main/java/tech/takahana/backgroundtaskusecase/feature/main/MainActivity.kt
+++ feature/main/src/main/java/tech/takahana/backgroundtaskusecase/feature/main/MainActivity.kt
@@ -1,38 +1,5 @@
 package tech.takahana.backgroundtaskusecase.feature.main
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import tech.takahana.backgroundtaskusecase.shared.feature.ui.theme.theme.BackgroundTaskUseCaseTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            BackgroundTaskUseCaseTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BackgroundTaskUseCaseTheme {
-        Greeting("Android")
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
