d04b4de0bf846ee0cf0df1981b48d9568a82eb9f
Go Takahana <tagahana508@gmail.com>
2021-07-24 10:07:48 +0900

    Add layout of RunWhenFragmentClosedFragment

--- feature/runwhenfragmentclosed/src/main/java/tech/takahana/backgroundtaskusecase/feature/runwhenfragmentclosed/ui/RunWhenFragmentClosedFragment.kt
+++ feature/runwhenfragmentclosed/src/main/java/tech/takahana/backgroundtaskusecase/feature/runwhenfragmentclosed/ui/RunWhenFragmentClosedFragment.kt
@@ -1,6 +1,6 @@
 package tech.takahana.backgroundtaskusecase.feature.runwhenfragmentclosed.ui
 
 import androidx.fragment.app.Fragment
+import tech.takahana.backgroundtaskusecase.feature.runwhenfragmentclosed.R
 
-class RunWhenFragmentClosedFragment : Fragment() {
-}
+class RunWhenFragmentClosedFragment : Fragment(R.layout.fragment_run_when_fragment_closed)
